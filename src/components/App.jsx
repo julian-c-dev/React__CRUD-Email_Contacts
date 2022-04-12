582dc771084bc400ca64f47eb37f74eaa278bb95
julian-c-dev <78905935+julian-c-dev@users.noreply.github.com>
2022-04-12 12:12:26 +0100

    Add data into LocalStorage

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,15 +1,20 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AddContact from "./AddContact";
 import "./App.css";
 import ContactList from "./ContactList";
 import Header from "./Header";
 
 function App() {
+  const LOCAL_STORAGE_KEY = "contacts";
   const [contacts, setContacts] = useState([]);
 
   const AddContactHandler = (name, email) => {
     setContacts([...contacts, { name, email }]);
   };
+
+  useEffect(() => {
+    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(contacts));
+  }, [contacts]);
 
   return (
     <div className="ui container">
